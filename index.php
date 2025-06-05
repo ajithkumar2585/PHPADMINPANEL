fc1400baf540264d49ef810bf7a6b5fafeb73df9
devopstest <devopstest@gmail.com>
2025-06-05 13:10:48 +0530

    "New commit"

--- index.php
+++ index.php
@@ -1,39 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Welcome To Forum</title>
-
-    <!-- Bootstrap core CSS -->
-    <link href="css/bootstrap.css" rel="stylesheet">
-    <!-- Custom styles for this template -->
-    <link href="css/custom.css" rel="stylesheet">
-  </head>
-
-  <body>
-
-    <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-      <div class="container">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="index.html">Forum</a>
-        </div>
-        <div class="collapse navbar-collapse">
-          <ul class="nav navbar-nav navbar-right">
-            <li class="active"><a href="index.html">Home</a></li>
-            <li><a href="register.html">Register</a></li>
-            <li><a href="create.html">Create Topic</a></li>
-          </ul>
-        </div><!--/.nav-collapse -->
-      </div>
-    </div>
+<?php require "includes/header.php;" ?>
 
     <div class="container">
 		<div class="row">
@@ -130,42 +95,7 @@
 					</div>
 				</div>
 			</div>
-			<div class="col-md-4">
-				<div class="sidebar">
+<?php require "includes/footer.php;" ?>
+			
 					
 					
-					<div class="block">
-					<h3>Categories</h3>
-					<div class="list-group block ">
-						<a href="#" class="list-group-item active">All Topics <span class="badge pull-right">14</span></a> 
-						<a href="#" class="list-group-item">Design<span class="color badge pull-right">4</span></a>
-						<a href="#" class="list-group-item">Development<span class="color badge pull-right">9</span></a>
-						<a href="#" class="list-group-item">Business & Marketing <span class="color badge pull-right">12</span></a>
-						<a href="#" class="list-group-item">Search Engines<span class="color badge pull-right">7</span></a>
-						<a href="#" class="list-group-item">Cloud & Hosting <span class="color badge pull-right">3</span></a>
-					</div>
-					</div>
-
-					<div class="block" style="margin-top: 20px;">
-						<h3 class="margin-top: 40px">Forum Statistics</h3>
-						<div class="list-group">
-							<a href="#" class="list-group-item">Total Number of Users:<span class="color badge pull-right">4</span></a>
-							<a href="#" class="list-group-item">Total Number of Topics:<span class="color badge pull-right">9</span></a>
-							<a href="#" class="list-group-item">Total Number of Categories: <span class="color badge pull-right">12</span></a>
-							
-						</div>
-				    </div>
-			    </div>	
-				</div>
-			</div>
-		</div>
-    </div><!-- /.container -->
-
-
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-    <script src="js/bootstrap.js"></script>
-  </body>
-</html>
