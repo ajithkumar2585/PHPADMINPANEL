7b2c06ce9cac2243e6323b4170defeb766791c51
devopstest <devopstest@gmail.com>
2025-06-05 16:45:18 +0530

    new register file

--- auth/register.php
+++ auth/register.php
@@ -1,4 +1,4 @@
-<?php require('includes/header.php'); ?>
+<?php require "../includes/header.php"; ?>
 
 <div class="container">
 		<div class="row">
@@ -46,27 +46,4 @@
 					</div>
 				</div>
 			</div>
-			<div class="col-md-4">
-				<div id="sidebar">
-					<div class="block">
-					
-					
-					<div class="block">
-					<h3>Categories</h3>
-					<div class="list-group">
-						<a href="#" class="list-group-item active">All Topics <span class="color badge pull-right">14</span></a> 
-						<a href="#" class="list-group-item">Design<span class="color badge pull-right">4</span></a>
-						<a href="#" class="list-group-item">Development<span class="color badge pull-right">9</span></a>
-						<a href="#" class="list-group-item">Business & Marketing <span class="color badge pull-right">12</span></a>
-						<a href="#" class="list-group-item">Search Engines<span class="color badge pull-right">7</span></a>
-						<a href="#" class="list-group-item">Cloud & Hosting <span class="color badge pull-right">3</span></a>
-					</div>
-				</div>	
-				</div>
-			</div>
-		</div>
-    </div><!-- /.container -->
-
-
-
-	<?php require('includes/footer.php'); ?>
+<?php require "../includes/footer.php"; ?>
